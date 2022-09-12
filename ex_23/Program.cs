379626df1c73aaa4f5c7c5c78f4535015f1fde2d
forbidden <forbidden@fedora.com>
2022-09-12 20:08:09 +0300

    add ex_23

---
+++ ex_23/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+    {
+        Console.WriteLine((i*i)*i);
+    }
